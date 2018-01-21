09f85b37953920a6d4dd84ed1cfe8058025a92ac
Karl Henkel <karlh07@gmail.com>
2018-01-21 16:12:04 -0500

    Hardcoded workaround for ignoring win key

---
+++ window-mover/ActionMap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parabox.WindowMover
+{
+	class ActionMap
+	{
+	}
+}
--- window-mover/WindowManager.cs
+++ window-mover/WindowManager.cs
@@ -3,6 +3,9 @@ using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using Gma.System.MouseKeyHook;
 using System.Drawing;
+using System.Collections.Generic;
+using WindowsInput;
+using WindowsInput.Native;
 
 namespace Parabox.WindowMover
 {
@@ -26,15 +29,29 @@ namespace Parabox.WindowMover
 		IntPtr m_DraggingWindowHandle;
 		Point m_CursorOffset;
 		Rectangle m_WindowRectOrigin;
+		IInputSimulator m_InputSimulator;
+
+		bool m_WindowKeyDown = false;
+
+		HashSet<Keys> m_IgnoreNextKeyUp;
 
 		public WindowManager(Form parent)
 		{
 			m_GlobalHook = Hook.GlobalEvents();
+
 			m_GlobalHook.MouseDownExt += OnMouseDownExt;
 			m_GlobalHook.MouseDragStartedExt += OnMouseDragStartedExt;
 			m_GlobalHook.MouseDragFinishedExt += MouseDragFinishedExt;
 			m_GlobalHook.MouseMoveExt += OnMouseMoveExt;
+
+			m_GlobalHook.KeyDown += OnKeyDown;
+			m_GlobalHook.KeyUp+= OnKeyUp;
+
 			m_ParentForm = parent;
+
+			m_IgnoreNextKeyUp = new HashSet<Keys>();
+
+			m_InputSimulator = new InputSimulator();
 		}
 
 		~WindowManager()
@@ -44,20 +61,55 @@ namespace Parabox.WindowMover
 			m_GlobalHook.MouseDragFinishedExt -= MouseDragFinishedExt;
 			m_GlobalHook.MouseMoveExt -= OnMouseMoveExt;
 
+			m_GlobalHook.KeyDown -= OnKeyDown;
+			m_GlobalHook.KeyUp -= OnKeyUp;
+
 			m_GlobalHook.Dispose();
+		}
+
+		void OnKeyDown(object sender, KeyEventArgs args)
+		{
+			//Console.WriteLine("down: " + args.KeyCode + " (" + args.Modifiers + ")");
+
+			if (args.KeyCode == Keys.LWin)
+			{
+				args.Handled = true;
+				m_WindowKeyDown = true;
+			}
+		}
+
+		void OnKeyUp(object sender, KeyEventArgs args)
+		{
+			if (args.KeyCode == Keys.LWin)
+			{
+				if (m_IgnoreNextKeyUp.Contains(args.KeyCode))
+				{
+					Console.WriteLine("ignore win");
+					m_IgnoreNextKeyUp.Remove(args.KeyCode);
+					args.Handled = true;
+				}
+				else
+				{
+					m_InputSimulator.Keyboard.KeyDown(VirtualKeyCode.LWIN);
+					m_InputSimulator.Keyboard.KeyUp(VirtualKeyCode.LWIN);
+				}
+
+				m_WindowKeyDown = false;
+			}
 		}
 
 		void OnMouseDownExt(object sender, MouseEventExtArgs args)
 		{
-			if (args.Button == MouseButtons.Left)
+			if (args.Button == MouseButtons.Left && m_WindowKeyDown)
 			{
+				m_IgnoreNextKeyUp.Add(Keys.LWin);
+
 				m_DraggingWindowHandle = WindowFromPoint(args.Location);
 				
 				if(m_DraggingWindowHandle != IntPtr.Zero && GetWindowRect(m_DraggingWindowHandle, ref m_WindowRectOrigin))
 				{
 					m_CursorOffset.X = args.X - m_WindowRectOrigin.Location.X;
 					m_CursorOffset.Y = args.Y - m_WindowRectOrigin.Location.Y;
-					args.Handled = true;
 				}
 			}
 		}
@@ -67,7 +119,6 @@ namespace Parabox.WindowMover
 			if (m_DraggingWindowHandle == IntPtr.Zero)
 				return;
 
-			args.Handled = true;
 			m_IsDragging = true;
 		}
 
@@ -76,7 +127,6 @@ namespace Parabox.WindowMover
 			if (!m_IsDragging)
 				return;
 
-			args.Handled = true;
 			m_IsDragging = false;
 			m_DraggingWindowHandle = IntPtr.Zero;
 		}
@@ -86,24 +136,23 @@ namespace Parabox.WindowMover
 			if (m_IsDragging)
 			{
 				if (SetWindowPos(m_DraggingWindowHandle,
-					(int) InsertWindowOrder.Top,
+					(int)InsertWindowOrder.Top,
 					args.X - m_CursorOffset.X,
 					args.Y - m_CursorOffset.Y,
 					m_ParentForm.Bounds.Width,
 					m_ParentForm.Bounds.Height,
-					(uint) (
+					(uint)(
 						SetWindowPositionFlags.NoSize |
 						SetWindowPositionFlags.NoZOrder |
 						SetWindowPositionFlags.ShowWindow
-					)) != IntPtr.Zero)
-				{
-					args.Handled = true;
-				}
-				else
+					)) == IntPtr.Zero)
 				{
 #if DEBUG
 					Console.WriteLine("Failed to set window position: " + GetLastError());
 #endif
+				}
+				else
+				{
 				}
 			}
 		}
