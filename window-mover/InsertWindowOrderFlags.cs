3fd9f247624c3a6363a7e468b7ae467ca3166d8a
Karl Henkel <karlh07@gmail.com>
2018-01-21 15:01:19 -0500

    Dragging windows with left mouse button working

---
+++ window-mover/InsertWindowOrderFlags.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parabox.WindowMover
+{
+	enum InsertWindowOrder
+	{
+		/// Places the window at the bottom of the Z order. If the hWnd parameter identifies a topmost window, the window loses its topmost status and is placed at the bottom of all other windows.
+		Bottom = 1,
+		/// Places the window above all non-topmost windows (that is, behind all topmost windows). This flag has no effect if the window is already a non-topmost window.
+		NoTopMost = -2,
+		/// Places the window at the top of the Z order.
+		Top = 0,
+		/// Places the window above all non-topmost windows. The window maintains its topmost position even when it is deactivated.
+		TopMost = -1,
+	}
+}
--- window-mover/WindowManager.cs
+++ window-mover/WindowManager.cs
@@ -9,45 +9,103 @@ namespace Parabox.WindowMover
 	class WindowManager
 	{
 		[DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-		public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, uint wFlags);
+		public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, uint wFlags);
+
+		[DllImport("user32.dll", EntryPoint = "GetWindowRect")]
+		public static extern bool GetWindowRect(IntPtr hWnd, ref Rectangle rect);
 
 		[DllImport("user32.dll", EntryPoint = "WindowFromPoint")]
 		public static extern IntPtr WindowFromPoint(Point point);
 
+		[DllImport("Kernel32.dll", EntryPoint = "GetLastError")]
+		public static extern uint GetLastError();
+		
 		IKeyboardMouseEvents m_GlobalHook;
 		Form m_ParentForm;
+		bool m_IsDragging;
+		IntPtr m_DraggingWindowHandle;
+		Point m_CursorOffset;
+		Rectangle m_WindowRectOrigin;
 
 		public WindowManager(Form parent)
 		{
 			m_GlobalHook = Hook.GlobalEvents();
-			m_GlobalHook.MouseDownExt += MouseDownExt;
+			m_GlobalHook.MouseDownExt += OnMouseDownExt;
+			m_GlobalHook.MouseDragStartedExt += OnMouseDragStartedExt;
+			m_GlobalHook.MouseDragFinishedExt += MouseDragFinishedExt;
+			m_GlobalHook.MouseMoveExt += OnMouseMoveExt;
 			m_ParentForm = parent;
 		}
 
 		~WindowManager()
 		{
-			m_GlobalHook.MouseDownExt -= MouseDownExt;
+			m_GlobalHook.MouseDownExt -= OnMouseDownExt;
+			m_GlobalHook.MouseDragStartedExt -= OnMouseDragStartedExt;
+			m_GlobalHook.MouseDragFinishedExt -= MouseDragFinishedExt;
+			m_GlobalHook.MouseMoveExt -= OnMouseMoveExt;
+
 			m_GlobalHook.Dispose();
 		}
 
-		void MouseDownExt(object sender, MouseEventExtArgs e)
+		void OnMouseDownExt(object sender, MouseEventExtArgs args)
 		{
-			Console.WriteLine("Mouse down: " + e.Button);
-
-			if (e.Button == MouseButtons.Left)
+			if (args.Button == MouseButtons.Left)
 			{
-				IntPtr hovered = WindowFromPoint(e.Location);
-
-				if(hovered == m_ParentForm.Handle)
-					SetWindowPos(m_ParentForm.Handle,
-						0,
-						0,
-						0,
-						m_ParentForm.Bounds.Width,
-						m_ParentForm.Bounds.Height,
-						(uint)(SetWindowPositionFlags.NoSize | SetWindowPositionFlags.NoZOrder));
+				m_DraggingWindowHandle = WindowFromPoint(args.Location);
+				
+				if(m_DraggingWindowHandle != IntPtr.Zero && GetWindowRect(m_DraggingWindowHandle, ref m_WindowRectOrigin))
+				{
+					m_CursorOffset.X = args.X - m_WindowRectOrigin.Location.X;
+					m_CursorOffset.Y = args.Y - m_WindowRectOrigin.Location.Y;
+					args.Handled = true;
+				}
 			}
 		}
 
+		void OnMouseDragStartedExt(object sender, MouseEventExtArgs args)
+		{
+			if (m_DraggingWindowHandle == IntPtr.Zero)
+				return;
+
+			args.Handled = true;
+			m_IsDragging = true;
+		}
+
+		void MouseDragFinishedExt(object sender, MouseEventExtArgs args)
+		{
+			if (!m_IsDragging)
+				return;
+
+			args.Handled = true;
+			m_IsDragging = false;
+			m_DraggingWindowHandle = IntPtr.Zero;
+		}
+
+		void OnMouseMoveExt(object sender, MouseEventExtArgs args)
+		{
+			if (m_IsDragging)
+			{
+				if (SetWindowPos(m_DraggingWindowHandle,
+					(int) InsertWindowOrder.Top,
+					args.X - m_CursorOffset.X,
+					args.Y - m_CursorOffset.Y,
+					m_ParentForm.Bounds.Width,
+					m_ParentForm.Bounds.Height,
+					(uint) (
+						SetWindowPositionFlags.NoSize |
+						SetWindowPositionFlags.NoZOrder |
+						SetWindowPositionFlags.ShowWindow
+					)) != IntPtr.Zero)
+				{
+					args.Handled = true;
+				}
+				else
+				{
+#if DEBUG
+					Console.WriteLine("Failed to set window position: " + GetLastError());
+#endif
+				}
+			}
+		}
 	}
 }
